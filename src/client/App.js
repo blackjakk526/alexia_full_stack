3058e58edd4943a27949559130562075c86378ac
Alan <agounley@gmail.com>
2019-11-08 16:07:07 -0500

    Added a few changes to try out functionality

--- src/client/App.js
+++ src/client/App.js
@@ -3,7 +3,10 @@ import './app.css';
 import ReactImage from './react.png';
 
 export default class App extends Component {
-  state = { username: null };
+  constructor(props) {
+    super(props);
+    this.state = { username: null };
+  }
 
   componentDidMount() {
     fetch('/api/getUsername')
@@ -15,7 +18,11 @@ export default class App extends Component {
     const { username } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        {username ? (
+          <h1>{`Hello ${username}`}</h1>
+        ) : (
+          <h1>Loading.. please wait!</h1>
+        )}
         <img src={ReactImage} alt="react" />
       </div>
     );
